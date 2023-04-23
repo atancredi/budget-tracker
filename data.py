3de42026eae7dd02bea5bf5199a186eadf128d9d
atancredi <alessandro.tancredi@aitechnologies.it>
2023-04-23 20:24:44 +0200

    data

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import sys
 class Database:
 
     conn = None
+    transactions = []
 
     def __init__(self, db_file):
         try:
@@ -11,6 +12,7 @@ class Database:
             if self.conn == None:
                 raise ConnectionError("Error connecting to database")
             self.create_transactions_table()
+            self.fetch_transactions()
         except (sqlite3.Error,ConnectionError) as e:
             print(e, file=sys.stderr)
 
@@ -27,6 +29,17 @@ class Database:
                                     ); """
         c = self.conn.cursor()
         c.execute(sql_create_transactions_table)
+    
+    def fetch_transactions(self):
+        with self.conn as c:
+            cur = c.cursor()
+            cur.execute('''SELECT * FROM transactions''')
+            for tr in cur.fetchall():
+                self.transactions.append(Transaction(tr))
+    
+    def dump_transactions(self):
+        for tr in self.transactions:
+            print(tr.__dict__)
 
 class Transaction:
     id: int
